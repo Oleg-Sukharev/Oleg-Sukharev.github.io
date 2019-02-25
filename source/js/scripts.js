d9417fd72a76827240c45abaf533a7919d1e4d78
Oleg <olegsukharev1991@gmail.com>
2019-02-26 01:04:50 +0200

    js

--- source/js/scripts.js
+++ source/js/scripts.js
@@ -1,5 +1,24 @@
-(function(){
+;(function(){
+  "use strict";
   $(document).ready(function(){
+
+  	var hideMenu = function(){
+		if($( ".navbar-collapse" ).hasClass( "show" )){
+			navbar.removeClass("show");
+		}
+  	};
+
+
+	$(".nav-link").on("click", function (evt) {
+		hideMenu();
+		evt.preventDefault();
+		var id  = $(this).attr('href');
+		var	top = $(id).offset().top;
+
+		$('body,html').animate({scrollTop: top}, 1500);
+	});
+
+
 
   });
 }());
