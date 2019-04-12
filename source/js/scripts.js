7f1b28a8e55e5d31536b1f4e5d4ba402d6199ac2
Oleg <olegsukharev1991@gmail.com>
2019-04-12 22:38:54 +0300

    new version

--- source/js/scripts.js
+++ source/js/scripts.js
@@ -1,71 +1,33 @@
 ;(function(){
-
-  $(document).ready(function(){
-    var headerHeight =  $( ".navbar" ).outerHeight();
-    function hideMenu(){
-		if($( ".navbar-collapse" ).hasClass( "show" )){
-		  $( ".navbar-collapse" ).removeClass("show");
-		}
-  	};
-
-	$(".nav-link").on("click", function (evt) {
-	   hideMenu();
-	   evt.preventDefault();
-	   var id  = $(this).attr('href');
-	   var	top = $(id).offset().top;
-
-	   $('body,html').animate({scrollTop: top - headerHeight}, 1500);
+	//preloader 
+	$(window).on("load", function (e) {
+	    $(".loader_inner").fadeOut();
+	    $(".loader").delay(500).fadeOut("slow");
 	});
 
-    jQuery.validator.addMethod("lettersonly", function(value, element){
-        return this.optional(element) || /^[a-z]+$/i.test(value);
-    }, "Letters only please");
+  	$(document).ready(function(){
+  		//burger menu
+		$.fatNav();
+		//animation 
+		AOS.init({
+       		easing: 'ease-in-out-sine'
+   		});
 
-	$('#contactForm').validate({
-        rules: {
-            name: {
-                required: true,
-                minlength: 3,
-                maxlength: 25,
-                lettersonly: true
-            },
-            email: {
-                required: true,
-                email: true
-            },
-            phone: {
-                required: true,
-                minlength: 7,
-                maxlength: 15,
-                number: true
 
-            },
-            message: {
-            	required: true
-            }
-        },
-        messages: {
-            name: {
-                required: "Please enter your name",
-                minlength: "Please enter at least 3 characters",
-                maxlength: "Please enter no more 20 characters"
-            },
-            email: {
-                required: "Please enter email",
-                email: "Please check your email is correct"
-            },
-            phone: {
-                required: "Please enter mobile phone number",
-                minlength: "Please enter at least 7 characters",
-                maxlength: "Please enter no more 15 characters",
-                digits: true
-            },
-            message: {
-                 required: "Please enter a message"
-            }
+
+	//filter porfolio
+    $(".portfolio__btn").click(function(){
+        var filterValue = $(this).attr('data-filter');
+        $(".portfolio__btn").removeClass("portfolio__btn-active");
+        $(this).addClass("portfolio__btn-active");
+
+        if(filterValue == "all"){
+            $(".all").show("slow");
+        }else{   
+            $(".all").not('.'+filterValue).hide();
+            $(".all").filter('.'+filterValue).show("slow");
         }
-    });
-
+    });    
 
   });
 }());
