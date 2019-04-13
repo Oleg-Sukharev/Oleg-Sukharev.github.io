dc528ed61a256f2b822f1d7314ed1135d1ca7552
Oleg <olegsukharev1991@gmail.com>
2019-04-14 01:59:38 +0300

    initial-scale

--- source/js/scripts.js
+++ source/js/scripts.js
@@ -29,5 +29,41 @@
         }
     });    
 
+   jQuery.validator.addMethod("lettersonly", function(value, element){
+        return this.optional(element) || /^[a-z]+$/i.test(value);
+    }, "Letters only please");
+
+  $('#contactForm').validate({
+        rules: {
+            name: {
+                required: true,
+                minlength: 3,
+                maxlength: 25,
+                lettersonly: true
+            },
+            email: {
+                required: true,
+                email: true
+            },
+            message: {
+              required: true
+            }
+        },
+        messages: {
+            name: {
+                required: "Please enter your name",
+                minlength: "Please enter at least 3 characters",
+                maxlength: "Please enter no more 20 characters"
+            },
+            email: {
+                required: "Please enter email",
+                email: "Please check your email is correct"
+            },
+            message: {
+                 required: "Please enter a message"
+            }
+        }
+    });
+
   });
 }());
