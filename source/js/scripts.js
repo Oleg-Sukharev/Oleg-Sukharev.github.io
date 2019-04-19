b411f17b1680b78434cd3dcc1c582439e69ffa34
Oleg <olegsukharev1991@gmail.com>
2019-04-19 18:39:51 +0300

    scroll

--- source/js/scripts.js
+++ source/js/scripts.js
@@ -1,13 +1,13 @@
 ;(function(){
 	//preloader 
-	$(window).on("load", function (e) {
+	$(window).on("load", function () {
 	    $(".loader_inner").fadeOut();
 	    $(".loader").delay(500).fadeOut("slow");
 	});
 
-  	$(document).ready(function(){
+  $(document).ready(function(){
 
-  		//burger menu
+  	//burger menu
 		$.fatNav();
 
 		//animation 
@@ -29,41 +29,43 @@
         }
     });    
 
-   jQuery.validator.addMethod("lettersonly", function(value, element){
-        return this.optional(element) || /^[a-z]+$/i.test(value);
+
+    //form validation
+    jQuery.validator.addMethod("lettersonly", function(value, element){
+          return this.optional(element) || /^[a-z]+$/i.test(value);
     }, "Letters only please");
 
-  $('#contactForm').validate({
-        rules: {
-            name: {
-                required: true,
-                minlength: 3,
-                maxlength: 25,
-                lettersonly: true
-            },
-            email: {
-                required: true,
-                email: true
-            },
-            message: {
-              required: true
-            }
-        },
-        messages: {
-            name: {
-                required: "Please enter your name",
-                minlength: "Please enter at least 3 characters",
-                maxlength: "Please enter no more 20 characters"
-            },
-            email: {
-                required: "Please enter email",
-                email: "Please check your email is correct"
-            },
-            message: {
-                 required: "Please enter a message"
-            }
-        }
-    });
-
+    $('#contactForm').validate({
+          rules: {
+              name: {
+                  required: true,
+                  minlength: 3,
+                  maxlength: 25,
+                  lettersonly: true
+              },
+              email: {
+                  required: true,
+                  email: true
+              },
+              message: {
+                required: true
+              }
+          },
+          messages: {
+              name: {
+                  required: "Please enter your name",
+                  minlength: "Please enter at least 3 characters",
+                  maxlength: "Please enter no more 20 characters"
+              },
+              email: {
+                  required: "Please enter email",
+                  email: "Please check your email is correct"
+              },
+              message: {
+                   required: "Please enter a message"
+              }
+          }
+      });
   });
+
 }());
