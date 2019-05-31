5a283d4c5136f4923110c46b56017e37f29e9227
Oleg <olegsukharev1991@gmail.com>
2019-06-01 00:12:47 +0300

    eslint

--- source/js/scripts.js
+++ source/js/scripts.js
@@ -1,137 +1,80 @@
-;(function() {
-//preloader
-  $(window).on(`load`, function(){
-    $(`.loader_inner`).fadeOut();
-    $(`.loader`).delay(500).fadeOut(`slow`);
-  });
+/* eslint-disable jquery/no-show */
+/* eslint-disable eqeqeq */
+/* eslint-disable jquery/no-class */
+/* eslint-disable jquery/no-ready */
+/* eslint-disable no-undef */
+/* eslint-disable jquery/no-filter */
+;(function () {
+// preloader
+  $(window).on(`load`, function () {
+    // eslint-disable-next-line jquery/no-fade
+    $(`.loader_inner`).fadeOut()
+    // eslint-disable-next-line jquery/no-fade
+    $(`.loader`).delay(500).fadeOut(`slow`)
+  })
 
   $(document).ready(function () {
   // burger menu
-  $.fatNav();
+    $.fatNav()
 
     // animation
     AOS.init({
-      easing: 'ease-in-out-sine',
-    });
+      easing: 'ease-in-out-sine'
+    })
 
     // filter porfolio
-    $(`.portfolio__btn`).click(function() {
-      const filterValue = $(this).attr('data-filter');
-      $(`.portfolio__btn`).removeClass(`portfolio__btn-active`);
-      $(this).addClass(`portfolio__btn-active`);
+    $(`.portfolio__btn`).click(function () {
+      // eslint-disable-next-line jquery/no-attr
+      const filterValue = $(this).attr('data-filter')
+      $(`.portfolio__btn`).removeClass(`portfolio__btn-active`)
+      $(this).addClass(`portfolio__btn-active`)
 
       if (filterValue == `all`) {
-        $(`.all`).show(`slow`);
+        $(`.all`).show(`slow`)
       } else {
-        $(`.all`).not('.'+filterValue).hide();
-        $(`.all`).filter('.'+filterValue).show(`slow`);
+        // eslint-disable-next-line jquery/no-hide
+        $(`.all`).not('.' + filterValue).hide()
+        $(`.all`).filter('.' + filterValue).show(`slow`)
       }
-    });
-
+    })
 
     // form validation
-    jQuery.validator.addMethod(`lettersonly`, function(value, element) {
-      return this.optional(element) || /^[a-z]+$/i.test(value);
-    }, `Letters only please`);
+    // eslint-disable-next-line no-undef
+    jQuery.validator.addMethod(`lettersonly`, function (value, element) {
+      return this.optional(element) || /^[a-z]+$/i.test(value)
+    }, `Letters only please`)
 
+    // eslint-disable-next-line no-undef
     $('#contactForm').validate({
       rules: {
         name: {
           required: true,
           minlength: 3,
           maxlength: 25,
-          lettersonly: true,
+          lettersonly: true
         },
         email: {
           required: true,
-          email: true,
+          email: true
         },
         message: {
-          required: true,
-        },
+          required: true
+        }
       },
       messages: {
         name: {
           required: `Please enter your name`,
           minlength: `Please enter at least 3 characters`,
-          maxlength: `Please enter no more 20 characters`,
+          maxlength: `Please enter no more 20 characters`
         },
         email: {
           required: `Please enter email`,
-          email: `Please check your email is correct`,
+          email: `Please check your email is correct`
         },
         message: {
-          required: `Please enter a message`,
-        },
-      },
-    });
-  });
-}()); ;(function() {
-// preloader
-  $(window).on(`load`, function() {
-    $(`.loader_inner`).fadeOut();
-    $(`.loader`).delay(500).fadeOut(`slow`);
-  });
-
-  $(document).ready(function() {
-  // burger menu
-    $.fatNav();
-
-    // animation
-    AOS.init({
-      easing: 'ease-in-out-sine',
-  });
-
-  // filter porfolio
-    $(`.portfolio__btn`).click(function() {
-      const filterValue = $(this).attr('data-filter');
-      $(`.portfolio__btn`).removeClass(`portfolio__btn-active`);
-      $(this).addClass(`portfolio__btn-active`);
-
-      if (filterValue == `all`) {
-      $(`.all`).show(`slow`);
-      } else {
-        $(`.all`).not('.'+filterValue).hide();
-        $(`.all`).filter('.'+filterValue).show(`slow`);
-    }
-    });
-
-
-    // form validation
-    jQuery.validator.addMethod(`lettersonly`, function(value, element) {
-      return this.optional(element) || /^[a-z]+$/i.test(value);
-    }, `Letters only please`);
-
-    $('#contactForm').validate({
-      rules: {
-        name: {
-          required: true,
-          minlength: 3,
-          maxlength: 25,
-          lettersonly: true,
-        },
-        email: {
-          required: true,
-          email: true,
-        },
-        message: {
-          required: true,
-        },
-      },
-      messages: {
-        name: {
-          required: `Please enter your name`,
-          minlength: `Please enter at least 3 characters`,
-          maxlength: `Please enter no more 20 characters`,
-        },
-        email: {
-          required: `Please enter email`,
-          email: `Please check your email is correct`,
-        },
-        message: {
-          required: `Please enter a message`,
-       },
-      },
-    });
-  });
-}());
+          required: `Please enter a message`
+        }
+      }
+    })
+  })
+}())
