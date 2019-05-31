8a579aca35bcc5c80e739f4f544f7031a5eabaf7
Oleg <olegsukharev1991@gmail.com>
2019-05-31 23:50:23 +0300

    editconfig

--- gulpfile.js
+++ gulpfile.js
@@ -1,40 +1,31 @@
-var gulp = require('gulp');
-var less = require('gulp-less');
-var plumber = require('gulp-plumber');
-var autoprefixer = require('gulp-autoprefixer');
-var server = require('browser-sync').create();
+const gulp = require('gulp');
+const less = require('gulp-less');
+const plumber = require('gulp-plumber');
+const autoprefixer = require('gulp-autoprefixer');
+const server = require('browser-sync').create();
 
 
-gulp.task('styles', function () {
-     gulp.src('source/less/style.less')
-        .pipe(plumber())
-        .pipe(less())
-        .pipe(autoprefixer({
-            browsers: ['last 2 versions'],
-            cascade: false
-        }))
-        .pipe(gulp.dest("source/css"))
-        .pipe(server.stream());
+gulp.task('styles', function() {
+  gulp.src('source/less/style.less')
+      .pipe(plumber())
+      .pipe(less())
+      .pipe(autoprefixer({
+        browsers: ['last 2 versions'],
+        cascade: false,
+      }))
+      .pipe(gulp.dest('source/css'))
+      .pipe(server.stream());
 });
 
 
-gulp.task("serve", ["styles"],function() {
-    server.init({
-            server: "source"
-    });
-    gulp.watch("source/less/**/*.less", ["styles"]);
-    gulp.watch("source/*.html")
-     .on("change",server.reload);
+gulp.task('serve', ['styles'], function() {
+  server.init({
+    server: 'source',
+  });
+  gulp.watch('source/less/**/*.less', ['styles']);
+  gulp.watch('source/*.html')
+      .on('change', server.reload);
 });
 
-gulp.task("default", ["styles"]);
-gulp.task("default", ["serve"]); 
-
-
-
-
-
-
-
-
- 
+gulp.task('default', ['styles']);
+gulp.task('default', ['serve']);
--- source/js/scripts.js
+++ source/js/scripts.js
@@ -1,71 +1,137 @@
-;(function(){
-	//preloader 
-	$(window).on("load", function () {
-	    $(".loader_inner").fadeOut();
-	    $(".loader").delay(500).fadeOut("slow");
-	});
-
-  $(document).ready(function(){
-
-  	//burger menu
-		$.fatNav();
-
-		//animation 
-		AOS.init({
-       		easing: 'ease-in-out-sine'
-   	});
-
-	  //filter porfolio
-    $(".portfolio__btn").click(function(){
-        var filterValue = $(this).attr('data-filter');
-        $(".portfolio__btn").removeClass("portfolio__btn-active");
-        $(this).addClass("portfolio__btn-active");
-
-        if(filterValue == "all"){
-            $(".all").show("slow");
-        }else{   
-            $(".all").not('.'+filterValue).hide();
-            $(".all").filter('.'+filterValue).show("slow");
-        }
-    });    
-
-
-    //form validation
-    jQuery.validator.addMethod("lettersonly", function(value, element){
-          return this.optional(element) || /^[a-z]+$/i.test(value);
-    }, "Letters only please");
-
-    $('#contactForm').validate({
-          rules: {
-              name: {
-                  required: true,
-                  minlength: 3,
-                  maxlength: 25,
-                  lettersonly: true
-              },
-              email: {
-                  required: true,
-                  email: true
-              },
-              message: {
-                required: true
-              }
-          },
-          messages: {
-              name: {
-                  required: "Please enter your name",
-                  minlength: "Please enter at least 3 characters",
-                  maxlength: "Please enter no more 20 characters"
-              },
-              email: {
-                  required: "Please enter email",
-                  email: "Please check your email is correct"
-              },
-              message: {
-                   required: "Please enter a message"
-              }
-          }
-      });
+;(function() {
+//preloader
+  $(window).on(`load`, function(){
+    $(`.loader_inner`).fadeOut();
+    $(`.loader`).delay(500).fadeOut(`slow`);
   });
 
+  $(document).ready(function () {
+  // burger menu
+  $.fatNav();
+
+    // animation
+    AOS.init({
+      easing: 'ease-in-out-sine',
+    });
+
+    // filter porfolio
+    $(`.portfolio__btn`).click(function() {
+      const filterValue = $(this).attr('data-filter');
+      $(`.portfolio__btn`).removeClass(`portfolio__btn-active`);
+      $(this).addClass(`portfolio__btn-active`);
+
+      if (filterValue == `all`) {
+        $(`.all`).show(`slow`);
+      } else {
+        $(`.all`).not('.'+filterValue).hide();
+        $(`.all`).filter('.'+filterValue).show(`slow`);
+      }
+    });
+
+
+    // form validation
+    jQuery.validator.addMethod(`lettersonly`, function(value, element) {
+      return this.optional(element) || /^[a-z]+$/i.test(value);
+    }, `Letters only please`);
+
+    $('#contactForm').validate({
+      rules: {
+        name: {
+          required: true,
+          minlength: 3,
+          maxlength: 25,
+          lettersonly: true,
+        },
+        email: {
+          required: true,
+          email: true,
+        },
+        message: {
+          required: true,
+        },
+      },
+      messages: {
+        name: {
+          required: `Please enter your name`,
+          minlength: `Please enter at least 3 characters`,
+          maxlength: `Please enter no more 20 characters`,
+        },
+        email: {
+          required: `Please enter email`,
+          email: `Please check your email is correct`,
+        },
+        message: {
+          required: `Please enter a message`,
+        },
+      },
+    });
+  });
+}()); ;(function() {
+// preloader
+  $(window).on(`load`, function() {
+    $(`.loader_inner`).fadeOut();
+    $(`.loader`).delay(500).fadeOut(`slow`);
+  });
+
+  $(document).ready(function() {
+  // burger menu
+    $.fatNav();
+
+    // animation
+    AOS.init({
+      easing: 'ease-in-out-sine',
+  });
+
+  // filter porfolio
+    $(`.portfolio__btn`).click(function() {
+      const filterValue = $(this).attr('data-filter');
+      $(`.portfolio__btn`).removeClass(`portfolio__btn-active`);
+      $(this).addClass(`portfolio__btn-active`);
+
+      if (filterValue == `all`) {
+      $(`.all`).show(`slow`);
+      } else {
+        $(`.all`).not('.'+filterValue).hide();
+        $(`.all`).filter('.'+filterValue).show(`slow`);
+    }
+    });
+
+
+    // form validation
+    jQuery.validator.addMethod(`lettersonly`, function(value, element) {
+      return this.optional(element) || /^[a-z]+$/i.test(value);
+    }, `Letters only please`);
+
+    $('#contactForm').validate({
+      rules: {
+        name: {
+          required: true,
+          minlength: 3,
+          maxlength: 25,
+          lettersonly: true,
+        },
+        email: {
+          required: true,
+          email: true,
+        },
+        message: {
+          required: true,
+        },
+      },
+      messages: {
+        name: {
+          required: `Please enter your name`,
+          minlength: `Please enter at least 3 characters`,
+          maxlength: `Please enter no more 20 characters`,
+        },
+        email: {
+          required: `Please enter email`,
+          email: `Please check your email is correct`,
+        },
+        message: {
+          required: `Please enter a message`,
+       },
+      },
+    });
+  });
 }());
